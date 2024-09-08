ca97ae9e1ee81a5caa9f529b9eca79be21db1055
Levi <115052769+leviobrabo@users.noreply.github.com>
2024-09-07 21:17:17 -0300

    ✨ | Atualizando link do acesso ao eventos

--- services/eventHistoric.ts
+++ services/eventHistoric.ts
@@ -3,7 +3,7 @@ import { apiService } from "./client"
 
 export const eventHistoricService = {
     async getPhases():Promise<HistoricalEvents> {
-        const response = await apiService.get('https://raw.githubusercontent.com/leviobrabo/fatoshisbot/main/fatoshistoricos/data/eventos.json')
+        const response = await apiService.get('https://raw.githubusercontent.com/leviobrabo/history-api/main/history_api/data/events/events-pt.json')
         return response.data
     }
 }
