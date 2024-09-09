fb856fd0ca6db95c668a8f885e7b654f5615a529
leviobrabo <carlosjunior20313@gmail.com>
2024-09-09 00:37:54 -0300

    add google ads

--- app/layout.tsx
+++ app/layout.tsx
@@ -7,7 +7,8 @@ import ReactQueryClientProvider from "@/context/ReactQueryClientProvider";
 import { Analytics } from '@vercel/analytics/react';
 import Script from 'next/script';
 
-const outfit = Outfit({subsets: ["latin"]});
+const outfit = Outfit({ subsets: ["latin"] });
+
 export const metadata: Metadata = {
   title: "Hoje na história",
   description: "Saiba o que o dia de hoje tem a nos contar historicamente.",
@@ -20,34 +21,39 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="pt-br">
-        <head>
-          <Script
-            strategy="afterInteractive"
-            src={`https://www.googletagmanager.com/gtag/js?id=G-SZP8KF5M0E`}
-          />
-          <Script
-            id="google-analytics"
-            strategy="afterInteractive"
-            dangerouslySetInnerHTML={{
-              __html: `
-                window.dataLayer = window.dataLayer || [];
-                function gtag(){dataLayer.push(arguments);}
-                gtag('js', new Date());
-                gtag('config', 'G-SZP8KF5M0E', {
-                  page_path: window.location.pathname,
-                });
-              `,
-            }}
-          />
-        </head>
-        <body className={`${outfit.className} flex flex-col min-h-screen justify-between bg-CustomAntiqueWhite dark:bg-CustomCharcoal transition-colors duration-1000`}>
-          <ReactQueryClientProvider>
-            <Header/>
-            {children}
-            <Footer/>
-          </ReactQueryClientProvider>
-          <Analytics />
-        </body>
-      </html>
+      <head>
+        <Script
+          strategy="afterInteractive"
+          src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-3470054444155641"
+          crossorigin="anonymous"
+        />
+        <Script
+          strategy="afterInteractive"
+          src={`https://www.googletagmanager.com/gtag/js?id=G-SZP8KF5M0E`}
+        />
+        <Script
+          id="google-analytics"
+          strategy="afterInteractive"
+          dangerouslySetInnerHTML={{
+            __html: `
+              window.dataLayer = window.dataLayer || [];
+              function gtag(){dataLayer.push(arguments);}
+              gtag('js', new Date());
+              gtag('config', 'G-SZP8KF5M0E', {
+                page_path: window.location.pathname,
+              });
+            `,
+          }}
+        />
+      </head>
+      <body className={`${outfit.className} flex flex-col min-h-screen justify-between bg-CustomAntiqueWhite dark:bg-CustomCharcoal transition-colors duration-1000`}>
+        <ReactQueryClientProvider>
+          <Header />
+          {children}
+          <Footer />
+        </ReactQueryClientProvider>
+        <Analytics />
+      </body>
+    </html>
   );
 }
